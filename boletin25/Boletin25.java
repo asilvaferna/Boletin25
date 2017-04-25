ebe3b940f6b43fe569a266cf01dd665edc0a2f0e
adri4silva <adri4silva@gmail.com>
2017-04-25 09:36:38 +0200

    Subido boletin25

---
+++ boletin25/Boletin25.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin25;
+
+/**
+ *
+ * @author Adri
+ */
+public class Boletin25 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Ejercicio1 ej1 = new Ejercicio1();
+        ej1.setVisible(true);
+        Ejercicio2 ej2 = new Ejercicio2();
+        ej2.setVisible(true);
+        Ejercicio3 ej3 = new Ejercicio3();
+        ej3.setVisible(true);
+        Ejercicio4 ej4 = new Ejercicio4();
+        ej4.setVisible(true);
+        Ejercicio5 ej5 = new Ejercicio5();
+        ej5.setVisible(true);
+    }
+    
+}
